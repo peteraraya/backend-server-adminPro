35c43cdd277632bc9a46a6e39114c47253643837
Peter <piteraraya@gmail.com>
2020-01-16 01:49:42 -0300

    Fin parcial sección 16 - Fin sección hospitales

--- routes/upload.js
+++ routes/upload.js
@@ -154,7 +154,7 @@ function subirPorTipo(tipo, id, nombreArchivo, res) {
                 return res.status(200).json({
                     ok: true,
                     mensaje: 'Imagen de médico actualizada',
-                    usuario: medicoActualizado
+                    medico: medicoActualizado
                 });
 
             })
@@ -188,7 +188,7 @@ function subirPorTipo(tipo, id, nombreArchivo, res) {
                 return res.status(200).json({
                     ok: true,
                     mensaje: 'Imagen de hospital actualizada',
-                    usuario: hospitalActualizado
+                    hospital: hospitalActualizado
                 });
 
             })
