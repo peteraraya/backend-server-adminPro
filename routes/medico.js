7cc06f60eeb71b61d65adc45276ccdfa1fc929ee
Peter <piteraraya@gmail.com>
2020-01-19 18:34:52 -0300

    Fin de sección 16 del curso , se agregan modificaciones en rutas

--- routes/medico.js
+++ routes/medico.js
@@ -43,6 +43,45 @@ app.get('/', (req, res, next) => {
 
             });
 });
+
+
+// ==========================================
+// Obtener Medico
+// ==========================================
+app.get('/:id',( req, res)=>{
+
+    var id = req.params.id;
+
+    Medico.findById(id)
+        .populate('usuario','nombre email img')
+        .populate('hospital')
+        .exec( (err,medico)=>{
+            // Validaciones
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar medico',
+                    errors: err
+                });
+            }
+
+            if (!medico) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'El medico con el id ' + id + ' no existe',
+                    errors: { message: 'No existe un medico con ese ID' }
+                });
+            }
+
+            // Si todo sale bien
+            res.status(200).json({
+                ok: true,
+                medico: medico
+            });
+
+
+        });
+});
 // ==========================================
 // Actualizar Medico
 // ==========================================
